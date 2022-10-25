037a84378335072b153731c7696a406da190ccd0
Yungbopark <yungbopark@gmail.com>
2022-10-25 23:17:24 +0900

    Initial commit

---
+++ src/main/java/org/example/controller/Test.java
@@ -0,0 +1,58 @@
+package org.example.controller;
+
+
+import org.example.entity.Board;
+import org.example.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class Test {
+
+
+    @Autowired
+private BoardMapper boardMapper;
+
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("Test.home");
+
+        return "index";
+    }
+
+
+    @RequestMapping("/boardList.do")
+    public String boardList(Model model) {
+
+        System.out.println("Test.boardList");
+
+        List<Board> lists = boardMapper.getLists();
+
+        System.out.println("lists = " + lists.size());
+
+
+
+        return "";
+
+
+        /*mybatis mapper  인식 불가 해결
+        * mapper xml 파일을 interface class의 package경로랑 똑같이,
+        * resource 폴더의 하위 폴더 구조로 동일하게 구성해 줌
+        * 그리고 나서 applicationContext.xml에서 dataSource를 인식 못하는 문제 발생
+        *
+        * 설정에서
+        *
+        * <bean id="dataSource" class="com.zaxxer.hikari.HikariDataSource" destroy-method="close">
+
+        * dataSource를 bean에 넣어주는 과정에서 초기화 필요
+        *
+        * <constructor-arg ref="hikariConfig"/>
+        *  */
+
+
+    }
+}
---
+++ src/main/java/org/example/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package org.example.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.entity.Board;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    public List<Board> getLists();
+    // 인터페이스 안에 추상 메서드로 정의 되어 있음
+
+}
